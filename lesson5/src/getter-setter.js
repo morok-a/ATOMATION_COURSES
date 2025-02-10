53df05c41308c7325d851aee8b7f8fd400e83c1a
morok-a <a.morokhvych@gmail.com>
2025-02-10 11:45:12 +0200

    fix comments

--- lesson5/src/getter-setter.js
+++ lesson5/src/getter-setter.js
@@ -1,6 +1,6 @@
 const author = {
     name: "Stephen King",
-    age: 77,
+    _age: 77,
     education: {
         university: "University of Maine",
         age: "1970",
@@ -9,21 +9,24 @@ const author = {
         }
     },
     books: ["Green Mile", "Doctor Sleep", "It"],
-    intro: function() {
-        console.log(`The name of the author is ${this.name}. He studied at ${this.education.university} at year ${this.education.age}`);
-    },
-    get fullInformation() {
+    fullInformation: function() {
         return `Author name: ${this.name}. His age: ${this.age}. His education: ${this.education.authorEducation}`;
     },
-    set updateAge(newAge) {
+    get age() {
+        return this._age;
+    },
+    set age(newAge) {
         if (typeof newAge === "number" && newAge > 0) {
-            this.age = newAge;
+            this._age = newAge;
         } else {
             console.log("Please enter a valid age.");
         }
     },
-    set addBook(newBook){
-        if(!this.books.includes(newBook)){
+    get booksList() {
+        return this.books.join(", ");
+    },
+    set addBook(newBook) {
+        if (!this.books.includes(newBook)) {
             this.books.push(newBook);
         } else {
             console.log("This book is already in the list");
@@ -31,9 +34,9 @@ const author = {
     }
 };
 
-console.log(author.fullInformation);
-author.updateAge = 78;
-console.log(author.fullInformation);
+console.log(author.fullInformation());
+author.age = 78;
+console.log(author.fullInformation());
 author.addBook = "Dark Tower";
 console.log(author.books);
 author.addBook = "It";
